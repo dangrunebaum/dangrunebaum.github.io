e39a52bbbf121fb87e0ba85398e54570426ea91b
dangrunebaum <42687809+dangrunebaum@users.noreply.github.com>
2019-05-16 13:53:24 -0400

    add my-tokyo project

---
+++ my-tokyo/points-json.js
@@ -0,0 +1,137 @@
+var pointsData = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": { "group_id": 1, "rolltext": "Home: Yoyogi Yamate Park House", "detailhtml": '<p>2-bedroom apartment we called home for 8 years. 5-17-1 Yoyogi, Shibuya, Tokyo.<\/p><iframe src="https://www.google.com/maps/embed?pb=!4v1554648107661!6m8!1m7!1sd13a8kgWMBNtjHZOaq0Chg!2m2!1d35.67564437401072!2d139.687744556083!3f126.38555156239745!4f18.420339581829026!5f0.7820865974627469" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>', "color": "blue", "radius": "7" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          139.6881365776062,
+          35.675435044619505
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": { "group_id": 1, "rolltext": "St. Joseph Daycare", "detailhtml": '<p>My son\'s daycare for 5 years. 5-16-4, Yoyogi, Shibuya, Tokyo.<\/p><iframe src="https://www.google.com/maps/embed?pb=!4v1554735647419!6m8!1m7!1soFCcuU_kP77h8L3SxK6LLA!2m2!1d35.67470091741946!2d139.6888633094869!3f274.85216415880103!4f16.36657466440748!5f0.7820865974627469" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>', "color": "blue", "radius": "7" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          139.68868911266327,
+          35.674694232653685
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": { "group_id": 1, "rolltext": "Hatashiro Elementary School", "detailhtml": '<p>My son\'s elementary school. 1-32-12 Hatsudai, Shibuya, Tokyo.<\/p><iframe src="https://www.google.com/maps/embed?pb=!4v1554737275427!6m8!1m7!1serByHhT9EiI4DAAKDzwpLw!2m2!1d35.67942766580963!2d139.6838148060384!3f183.88399350226356!4f6.329899740173147!5f0.7820865974627469" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>', "color": "blue", "radius": "7" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          139.68430638313293,
+          35.679104141332566
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": { "group_id": 2, "rolltext": "Shibuya Sports Center", "detailhtml": '<p>Athletic facility with a soccer field where my son participated in kids soccer and swimming pool we used regularly. 1-40-18 Nishinara, Shibuya, Tokyo<\/p><iframe src="https://www.google.com/maps/embed?pb=!4v1554737486102!6m8!1m7!1sQCTDl_gdJt7B4BdiHlcGeg!2m2!1d35.67543622401688!2d139.6806106248964!3f36.12774253592089!4f13.169557805813639!5f0.7820865974627469" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>', "color": "fuchsia", "radius": "7" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+		  139.68124866485596,
+          35.676158419079826
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": { "group_id": 2, "rolltext": "OK Store", "detailhtml": '<p>Go-to grocery store. 2-5-7 Hatsudai, Shibuya, Tokyo.<\/p><iframe src="https://www.google.com/maps/embed?pb=!4v1554737640929!6m8!1m7!1sHShD9yXV6ck2I9P4PNbufg!2m2!1d35.67419482529063!2d139.686268452537!3f1.1751039590171093!4f6.4290846669501605!5f0.7820865974627469" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>', "color": "fuchsia", "radius": "7" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          139.68605518341064,
+          35.67443276796514
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": { "group_id": 1, "rolltext": "NHK Broadcasting Center", "detailhtml": '<p>My workplace, Japan\'s public broadcaster. 2-2-1 Jinnan, Shibuya, Tokyo.<\/p><iframe src="https://www.google.com/maps/embed?pb=!4v1554738068060!6m8!1m7!1sZiiUdKpEG1yNSsqlaj5PWA!2m2!1d35.66490561496975!2d139.6945330340855!3f60.31784575069264!4f13.998176020525264!5f0.7820865974627469" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>', "color": "blue", "radius": "7" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          139.6960973739624,
+          35.664845137892684
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": { "group_id": 2, "rolltext": "Yoyogi Park", "detailhtml": '<p>Iconic park in central Tokyo. 2-1 Yoyogikamizonocho, Shibuya, Tokyo.<\/p><iframe src="https://www.google.com/maps/embed?pb=!4v1554738343922!6m8!1m7!1serp2tYQzVW2KVkYGFgKIUg!2m2!1d35.67111394419427!2d139.6952303976405!3f165.72814782935419!4f-10.082824644859755!5f0.7820865974627469" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>', "color": "fuchsia", "radius": "7" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          139.69493865966794,
+          35.671713484426924
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": { "group_id": 2, "rolltext": "Yoyogi Koen Station", "detailhtml": '<p>Most frequently used subway station. Chiyoda Line.<\/p><iframe src="https://www.google.com/maps/embed?pb=!4v1554738506585!6m8!1m7!1sTfGqYoad4Q4BOEpRKudQ7A!2m2!1d35.66895846180118!2d139.6909176901987!3f304.09830974866713!4f0!5f0.7820865974627469" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>', "color": "fuchsia", "radius": "7" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          139.6913123130798,
+          35.66891566332578
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": { "group_id": 2, "rolltext": "Shibuya Station", "detailhtml": '<p>Famous retail hub and shopping district. 2-24 Shibuya, Shibuya, Tokyo.<\/p><iframe src="https://www.google.com/maps/embed?pb=!4v1554863335885!6m8!1m7!1sCAoSLEFGMVFpcFBydXRFcmZ0MFdJYy1sSnh4Z2FaUlJrZ3lxbGNLUkJhSzlGZi03!2m2!1d35.65963904433524!2d139.7004738159733!3f214.02517829040127!4f11.680309919504921!5f0.7820865974627469" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>', "color": "fuchsia", "radius": "7" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          139.70184803009033,
+          35.65921404753565
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": { "group_id": 1, "rolltext": "Symantec Japan", "detailhtml": '<p>My wife\'s workplace. Infoss Tower, 20−1 Sakuragaokacho, Shibuya, Tokyo.<\/p><iframe src="https://www.google.com/maps/embed?pb=!4v1554738808247!6m8!1m7!1sY2eKTYtniIhIp2ksygNWxg!2m2!1d35.65586236046827!2d139.7005721547023!3f155.86047496605187!4f28.36315152175598!5f0.7820865974627469" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>', "color": "blue", "radius": "7" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          139.7008717060089,
+          35.65522148593179
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": { "group_id": 2, "rolltext": "Tower Records Shibuya", "detailhtml": '<p>Favorite shopping spot and purportedly the world\'s largest record shop. 1-22-14 Jinnan, Shibuya, Tokyo.<\/p><iframe src="https://www.google.com/maps/embed?pb=!4v1554739104547!6m8!1m7!1suO0tj15aQOuCvkbQvlHjig!2m2!1d35.66225346812284!2d139.700817077988!3f149.45580231072358!4f28.065789034668853!5f0.7820865974627469" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>', "color": "fuchsia", "radius": "7" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          139.70109969377518,
+          35.6618879932842
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": { "group_id": 2, "rolltext": "Yoyogi Hachimangu Shrine", "detailhtml": '<p>Lovely ancient Shinto shrine. 5-1-1 Yoyogi, Shibuya, Tokyo.<\/p><iframe src="https://www.google.com/maps/embed?pb=!4v1554739263672!6m8!1m7!1sCAoSLEFGMVFpcFBDaWNBR053V0JoekxOUjBCSi1hSlNhUkJuNHBXNnp1WHNjTi05!2m2!1d35.67181333206656!2d139.6888850137635!3f359.8535816506926!4f-0.4443000338750238!5f0.7820865974627469" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>', "color": "fuchsia", "radius": "7" },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          139.68859255313873,
+          35.67210569450093
+        ]
+      }
+    }
+  ]
+}
