94ca4110ecb5a00fa065b3135edccad82ecc2773
Dan Grunebaum <42687809+dangrunebaum@users.noreply.github.com>
2019-12-07 12:13:34 -0500

    Add files via upload

---
+++ fender-vs-gibson/met-mobile/js/sound.js
@@ -0,0 +1,141 @@
+let samples;
+let playSoundBrand = null;
+
+function preload(){
+
+    samples = {
+        fender: {
+            fft: null,
+            top: 10,
+            left: 73,
+            sound: loadSound('sound/fender_sample.mp3')
+        },
+        gibson: {
+            fft: null,
+            top: 210,
+            left: 73,
+            sound: loadSound('sound/gibson_sample.mp3')
+        }
+    }
+  }
+  
+  function setup() {
+
+    let cnv = createCanvas(250, 375);
+    cnv.parent("canvas");
+    background(0);
+
+    samples.fender.fft = new p5.FFT();
+    samples.fender.sound.amp(0.2);
+    samples.gibson.fft = new p5.FFT();
+    samples.gibson.sound.amp(0.2);
+
+     //rectangles around sound samples, lines inside  
+     stroke(255);
+     strokeWeight(2);
+     fill(0);
+    rFender = rect(70, 10, 150, 150);
+    rGibson = rect(70, 210, 150, 150);
+
+    //text and lines inside sound squares 
+    textFont("Futura");
+    noStroke();
+    textSize(12);
+    textAlign(LEFT);
+    fill(255);
+    text('Click to play/pause \nFender Stratocaster', 77, 30);
+    text('Click to play/pause \nGibson Les Paul', 77, 230);
+    strokeWeight(1);
+    stroke(221, 105, 103); 
+    line(73, 85, 216, 85);
+    line(73, 285, 216, 285);
+    
+    //  samples.fender.top = WTopMargin + 350;
+    //  rect(798, WTopMargin + 648, 202, 202);
+    //  samples.gibson.top = WTopMargin + 650;
+    //  strokeWeight(1);
+    //  stroke(0, 73, 219);
+    //  line(802, WTopMargin + 455, 996, WTopMargin + 455);
+    //  line(802, WTopMargin + 755, 996, WTopMargin + 755);
+    //  stroke(221, 105, 103);
+    //  line(802, WTopMargin + 445, 996, WTopMargin + 445);
+    //  line(802, WTopMargin + 745, 996, WTopMargin + 745);
+}
+let rFender;
+let rGibson;
+function mouseClicked() {
+
+    // mouse positions for sound stuff 
+    if (mouseX >= 70 && mouseX <= 220) {
+        if (mouseY >= 10 && mouseY <= 160) {
+            togglePlay("fender");
+            // fill(0);
+            // rect(798, topMargin + 2448, 202, 202);
+            stroke(162, 224, 184);
+            strokeWeight(6);
+            // rect(70, 10, 150, 150);
+            // samples.fender.top = 10;
+        }
+        else if (mouseY >= 210 && mouseY <= 360) {
+            togglePlay("gibson");
+            // fill(0);
+            // rect(798, topMargin + 2748, 202, 202);
+            stroke(183, 132, 67);
+            strokeWeight(6);
+            // rect(798, WTopMargin + 648, 202, 202);
+            // samples.gibson.top = 210;
+        }
+    }
+   
+    // console.log(mouseX,mouseY);
+}
+
+//sound functions 
+function draw() {
+
+    //   background(0);
+    if (playSoundBrand === null) return;
+    let sample = samples[playSoundBrand];
+    let spectrum = sample.fft.analyze();
+    noStroke();
+    fill(0, 73, 219); // audio frequency spectrum is blue
+    for (var i = 0; i < spectrum.length; i++) {
+        let x = map(i, 0, spectrum.length, 0, 150);
+        let h = -150 + map(spectrum[i], 0, 255, 150, 0);
+        rect(x + sample.left, 147 + sample.top, 150 / spectrum.length, h);
+    }
+
+    let waveform = sample.fft.waveform();
+    noFill();
+    beginShape();
+    stroke(221, 105, 103); // amplitude waveform is red
+    strokeWeight(1);
+    for (var i = 0; i < waveform.length; i++) {
+        let x = map(i, 0, waveform.length, 0, 144);
+        let y = map(waveform[i], -1, 1, 0, 144);
+        vertex(x + sample.left, y + sample.top);
+    }
+    endShape();
+
+    // textFont("Futura");
+    // textSize(14);
+    // textAlign(LEFT);
+    // fill(255);
+    // text('click to play/pause \nFender Stratocaster', 75, 15);
+    // text('click to play/pause \nGibson Les Paul', 75, 225);
+}
+
+// fade sound if mouse is over canvas
+function togglePlay(brand) {
+    let sound = samples[brand].sound;
+    if (playSoundBrand !== null) {
+        if (sound.isPlaying()) {
+            sound.pause();
+            playSoundBrand = null;
+        }
+    } else {
+        playSoundBrand = brand;
+        sound.loop();
+    }
+
+}
