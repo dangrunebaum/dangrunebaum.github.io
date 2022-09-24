113f3ce4c13ee95f521464ebac056cbf61f7827b
daniel grunebaum <grund302@newschool.edu>
2022-09-23 20:07:21 -0400

    post virtual arp

---
+++ virtual-arp/sampleArrays.js
@@ -0,0 +1,29 @@
+// const arpArray = 
+
+const Arp1 = [
+'1.wav',
+'2.wav',
+'3.wav',
+'4.wav',
+'5.wav',
+'6.wav',
+'7.wav',
+'8.wav',
+'9.wav',
+'10.wav',
+'11.wav'
+];
+
+const Arp2 = [
+    '1.wav',
+    '2.wav',
+    '3.wav',
+    '4.wav',
+    '5.wav',
+    '6.wav',
+    '7.wav',
+    '8.wav',
+    '9.wav',
+    '10.wav',
+    '11.wav'
+    ];
