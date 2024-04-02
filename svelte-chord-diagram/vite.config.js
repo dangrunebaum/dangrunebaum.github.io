3772270dafd7aebae8d701b0b35ebcc0c314a31c
daniel grunebaum <grund302@newschool.edu>
2024-04-02 19:05:20 -0400

    delete chord-diagram

--- svelte-chord-diagram/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import { svelte } from "@sveltejs/vite-plugin-svelte";
-import path from "path";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: "/svelte-chord-diagram/", // change to your repo name
-  plugins: [svelte()],
-})
